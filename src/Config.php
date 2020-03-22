126cc5f1aeff2eb38979936af7c26da7b3ec7294
rezasrk <rezasarlak.007@gmail.com>
2020-03-22 22:39:18 +0430

    add type to whitlistattr

--- src/Config.php
+++ src/Config.php
@@ -32,7 +32,7 @@ class Config
 
 
     protected $withListAttr = [
-        'class', 'id', 'action', 'method', 'enctype', 'name', 'value','rows','multiple'
+        'class', 'id', 'action', 'method', 'enctype', 'name', 'value','rows','multiple','type'
     ];
 
     protected $labels;

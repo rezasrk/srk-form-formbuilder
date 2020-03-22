117afce1a98101b60346e0e1fc3eb88f4226f8a2
rezasrk <rezasarlak.007@gmail.com>
2020-03-22 21:04:27 +0430

    merge local formbuilder library in first project to github repositroy

---
+++ src/StaticElement.php
@@ -0,0 +1,100 @@
+<?php
+namespace SrkForm\FormBuilder;
+
+class StaticElement extends Config
+{
+    protected $is_start;
+
+    public function __construct(string $validation = "", bool $labelsFile = false)
+    {
+        if ($validation != "")
+            $this->is_start = (new $validation)->rules();
+        if ($labelsFile)
+            $this->labels = __('validation.attributes');
+    }
+
+    public function startRow()
+    {
+        $this->form .= "<div class='row'>";
+        return $this;
+    }
+
+    public function endRow()
+    {
+        $this->form .= "</div>";
+        return $this;
+    }
+
+    protected function beforeElement($width, $type = 'colMd')
+    {
+        switch ($type) {
+            case 'colXm':
+                $col = $this->colXs[$width];
+                break;
+            case 'colMd':
+                $col = $this->colMd[$width];
+                break;
+            case 'colSm':
+                $col = $this->colSm[$width];
+                break;
+            case 'colLg':
+                $col = $this->colLg[$width];
+                break;
+        }
+        $this->form .= "<div class='{$col}'><div class='form-group'>";
+    }
+
+    protected function afterElement()
+    {
+        $this->form .= "</div></div>";
+    }
+
+    protected function setStarRequired()
+    {
+        $this->form .= "<span class='text-danger'>*</span>" . str_repeat(' ', '1');
+    }
+
+    protected function label($labelName)
+    {
+        $this->form .= "<label>" . $labelName . "</label>";
+    }
+
+    protected function getAttribute(array $option, $beforeElement = true,$firstLabel = true)
+    {
+
+        if ($beforeElement) {
+            (array_key_exists('width', $option)) ? $width = $option['width'] : $width = 3;
+            $this->beforeElement($width);
+        }
+        $attribute = "";
+        if (count($option) != 0) {
+            foreach ($option as $key => $value) {
+                if (in_array($key, $this->withListAttr))
+                    $attribute .= $key . "='" . $value . "'" . str_repeat(' ', 1);
+
+                if ($key == "name" && !array_key_exists('star', $option) && is_array($this->is_start) && array_key_exists($value, $this->is_start) && $this->is_start[$value][0] == 'required')
+                    $this->setStarRequired();
+                elseif ($key == "star" && array_key_exists('star', $option))
+                    $this->setStarRequired();
+
+                if ($key == "name" && !array_key_exists('label', $option) && $firstLabel && is_array($this->labels) && array_key_exists($value, $this->labels))
+                    $this->label($this->labels[$value]);
+                elseif ($key == 'label' && array_key_exists('label', $option) && $firstLabel)
+                    $this->label($option['label']);
+
+            }
+        }
+
+        return $attribute;
+    }
+
+
+    protected function changeSortLabel(array $option = array(), $firstLabel)
+    {
+        if(array_key_exists('label',$option) && !$firstLabel)
+            $this->form .= $this->label($option['label']);
+        elseif(array_key_exists('name',$option) && array_key_exists($option['name'],$this->labels))
+            $this->form .= $this->label($this->labels[$option['name']]);
+
+    }
+}
